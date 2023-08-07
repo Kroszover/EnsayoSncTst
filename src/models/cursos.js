6215b86c292f2982f4468ef87221ee995529b1f8
Camilo Lavado <93832685+Kroszover@users.noreply.github.com>
2023-08-07 10:42:49 -0400

    a

---
+++ src/models/cursos.js
@@ -0,0 +1,26 @@
+import { DataTypes } from "sequelize";
+import { sequelize } from "../database/database.js";
+
+const Curso = sequelize.define(
+  "Curso",
+  {
+    codigo_curso: {
+      type: DataTypes.STRING(30),
+      primaryKey: true,
+    },
+    fecha_inicio: {
+      type: DataTypes.DATE,
+    },
+    fecha_termino: {
+      // Corrected the attribute name here
+      type: DataTypes.DATE,
+    },
+  },
+  {
+    tableName: "cursos",
+    updatedAt: false,
+    createdAt: false,
+  }
+);
+
+export default Curso;
